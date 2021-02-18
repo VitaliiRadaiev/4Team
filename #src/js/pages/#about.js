bf1c5c07c23cf549346c1435aff63f99435c54c6
Vitalii Radaiev <hf.chelenger@gamil.com>
2021-02-18 12:59:16 +0200

    fix

--- #src/js/pages/#about.js
+++ #src/js/pages/#about.js
@@ -1,19 +1,53 @@
-{
-    let textAnimationBlocks = document.querySelectorAll('.textAnimation');
-    if(textAnimationBlocks.length > 0) {
-        textAnimationBlocks.forEach(item => {
-            let textEl = item.querySelector('.typed');
-            let textStr = item.querySelector('.typed-strings');
+// {
+//     let textAnimationBlocks = document.querySelectorAll('.textAnimation');
+//     if(textAnimationBlocks.length > 0) {
+//         textAnimationBlocks.forEach(item => {
+//             let textEl = item.querySelector('.typed');
+//             let textStr = item.querySelector('.typed-strings');
 
-            let typed = new Typed(textEl, {
-                stringsElement: textStr,
-                typeSpeed: 100,
-                backSpeed: 0,
-                backDelay: 500,
-                startDelay: 1000,
-                loop: true,
-                cursorChar: '',
-            })
-        })
-    }
-}
+//             let typed = new Typed(textEl, {
+//                 stringsElement: textStr,
+//                 typeSpeed: 10,
+//                 backSpeed: 0,
+//                 backDelay: 4000,
+//                 startDelay: 1000,
+//                 loop: true,
+//                 cursorChar: '',
+//             })
+//         })
+//     }
+// }
+
+// Wrap every letter in a span
+let textWrapper = document.querySelector('.textAnimation .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.textAnimation .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.textAnimation .line',
+    translateX: [0, document.querySelector('.textAnimation .letters').getBoundingClientRect().width + 20],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.textAnimation .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.textAnimation',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
