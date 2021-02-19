1f3eb95b07bfd19e517bcf9070490bcde3e777c7
Vitalii Radaiev <hf.chelenger@gamil.com>
2021-02-19 09:55:28 +0200

    fix

--- #src/js/pages/#about.js
+++ #src/js/pages/#about.js
@@ -19,35 +19,75 @@
 // }
 
 // Wrap every letter in a span
-let textWrapper = document.querySelector('.textAnimation .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+let textWrapper = document.querySelectorAll('.textAnimation .typed-strings');
+if(textWrapper.length) {
+  let tl = anime.timeline({loop: true})
+  textWrapper.forEach(text => {
+    let letters = text.querySelector('.letters');
+    let replaceLetter = new Promise(resolve => {
+        letters.innerHTML = letters.textContent.replace(/./g, "<span class='letter'>$&</span>");
+        resolve();
+    }).then(() => {
+      tl.add({
+        targets: text.querySelector('.line'),
+        scaleY: [0,1],
+        opacity: [0.5,1],
+        easing: "easeOutExpo",
+        duration: 700
+      })
+      .add({
+        targets: text.querySelector('.line'),
+        translateX: [0, text.querySelector('.letters').getBoundingClientRect().width + 15],
+        easing: "easeOutExpo",
+        duration: 700,
+        delay: 100
+      }).add({
+        targets: text.querySelectorAll('.letter'),
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=775',
+        delay: (el, i) => 34 * (i+1)
+      }).add({
+        targets: text,
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+      });
+    })
+    
 
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.textAnimation .line',
-    scaleY: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700
   })
-  .add({
-    targets: '.textAnimation .line',
-    translateX: [0, document.querySelector('.textAnimation .letters').getBoundingClientRect().width + 20],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: 100
-  }).add({
-    targets: '.textAnimation .letter',
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=775',
-    delay: (el, i) => 34 * (i+1)
-  }).add({
-    targets: '.textAnimation',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+}
+//textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+
+// let tl = anime.timeline({loop: false})
+//   tl.add({
+//     targets: '.textAnimation .line',
+//     scaleY: [0,1],
+//     opacity: [0.5,1],
+//     easing: "easeOutExpo",
+//     duration: 700
+//   })
+//   .add({
+//     targets: '.textAnimation .line',
+//     translateX: [0, document.querySelector('.textAnimation .letters-1').getBoundingClientRect().width + 20],
+//     easing: "easeOutExpo",
+//     duration: 700,
+//     delay: 100
+//   }).add({
+//     targets: '.textAnimation .letters-1 .letter',
+//     opacity: [0,1],
+//     easing: "easeOutExpo",
+//     duration: 600,
+//     offset: '-=775',
+//     delay: (el, i) => 34 * (i+1)
+//   }).add({
+//     targets: '.textAnimation',
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
